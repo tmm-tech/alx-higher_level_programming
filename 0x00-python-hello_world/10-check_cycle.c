fcb9c799a073c3705656ec96f2c4feee6cae418b
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-01-03 02:13:58 +0300

    python high level

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int checkcycle(listint_t *list)
+int check_cycle(listint_t  *list)
 {
     listint_t *slow = list;
     listint_t *fast = list;
