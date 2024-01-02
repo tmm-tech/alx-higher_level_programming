fee4bc20c43ec0ee9b5607bbca4c5fdf423a937b
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-01-03 02:12:37 +0300

    python high level

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * chec_cycle - checks if a linked list contains a cycle
+ * check_cycle - checks if a linked list contains a cycle
  * @list: linked list to check
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
