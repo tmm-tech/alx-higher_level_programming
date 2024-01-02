d24641066521c3e2ae55322b1d99474f46ba73b9
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-01-03 02:01:18 +0300

    python high level

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(str[39:67] + str[187:112] + str[:6])
+print(str[39:67] + str[107:112] + str[:6])
 print(str)
