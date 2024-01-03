86ebd5b52a37445b02d159bea4ea753d075fd912
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-01-04 02:56:00 +0300

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # 100-print_tebahpla.py
+# Tony Mwangi Mugi <tonymugi074@gmail.com>
 
 for char in range(ord('z'), ord('A') - 1, -1):
     print("{}".format(chr(char)), end='' if char % 2 == 0 else chr(char - 32))
