0d30f56803bde7d6f7d0e0f0cb7f412913858442
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-01-07 21:08:50 +0300

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    newStr = ""
-    for letter in my_string:
-        if (letter != 'c' and letter != 'C'):
-            newStr += letter
-    return (newStr)
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
